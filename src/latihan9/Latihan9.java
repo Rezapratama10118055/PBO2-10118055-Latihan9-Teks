9473cd89073e4d77c48f8bc171a16813cc89eba5
User <User@DESKTOP-2RC2UP5>
2019-09-25 08:50:18 +0700

    first commit

---
+++ src/latihan9/Latihan9.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan9;
+
+/**
+ *
+ * @author User
+ * NAMA:REZA PRATAMA
+ * KELAS:PBO2
+ * NIM:10118055
+ * Deskripsi:program ini berisi program untuk menampilkan
+ * jenis jenis data bilangan bulat
+ */
+public class Latihan9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        char c;
+        char tab ='\t';
+        char newline ='\n';
+        c='A';
+        System.out.println("HASIL"+newline+"char:"+tab+"berisi"+c);
+             
+                
+    }
+    
+}
